a2173fa7b78a46ed6619a0b716097edd9c2f8323
borsodi-koppany <borsodi.koppany@students.jedlik.eu>
2025-11-14 08:35:44 +0100

    .env and connection to mongoose

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB successfully');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
